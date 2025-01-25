de2e332083b9669163ed255e52da7fa176c1d1c2
Vinoth <106597409+vinothjack@users.noreply.github.com>
2025-01-25 18:56:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,20 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://realestate-task.onrender.com",
-        changeOrigin: true,
-        secure: false,
+ server: {
+  proxy: {
+    "/api": {
+      target: "https://realestate-task.onrender.com",
+      changeOrigin: true,
+      secure: false,
+      configure: (proxy, options) => {
+        proxy.on("proxyReq", (proxyReq, req, res) => {
+          console.log(`[Proxy Request] ${req.method} ${req.url}`);
+        });
       },
     },
   },
+},
 
   plugins: [react()],
 });
