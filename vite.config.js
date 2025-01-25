6764219570bc4702efa72400f975a1e12e314ca8
Vinoth <106597409+vinothjack@users.noreply.github.com>
2025-01-25 18:22:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://realestate-task.onrender.com",
+        changeOrigin: true,
         secure: false,
       },
     },
